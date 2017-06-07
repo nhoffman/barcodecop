437f6706881be1cb89da4b5d218ebcee13be2a98
Tim Holland <timholl@uw.edu>
2017-06-07 14:19:15 -0700

    Add argument for qual score offset

--- barcodecop/__init__.py
+++ barcodecop/__init__.py
@@ -1,5 +1,9 @@
 from os import path
 
+# default values
+MIN_QUAL = 26
+QUAL_OFFSET = 33  # default is set for Illumina reads
+
 try:
     with open(path.join(path.dirname(__file__), 'data', 'ver')) as f:
         __version__ = f.read().strip().replace('-', '+', 1).replace('-', '.')
